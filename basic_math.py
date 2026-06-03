ed1087b278f5299c8090f4e85618395056a75f22
vmzwb96cc6-stack <nikunj_parmar@icloud.com>
2026-06-03 11:36:51 +0530

    COmmit

--- basic_math.py
+++ basic_math.py
@@ -3,3 +3,12 @@ b = 4
 # Addition
 c = a + b
 print(f"{a} + {b} the answer is : {c}")
+# Subtraction
+c = a - b
+print(f"{a} - {b} the answer is : {c}")
+# Multiplication
+c = a * b
+print(f"{a} * {b} the answer is : {c}")
+# Division
+c = a / b
+print(f"{a} / {b} the answer is : {c}")
